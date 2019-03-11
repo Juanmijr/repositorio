07326335e797dd92abb98a026395ad1ddb3bd43b
diurno <sam@google.com>
2019-03-11 09:50:57 +0100

    ssss

--- openarray/src/array.java
+++ openarray/src/array.java
@@ -4,6 +4,7 @@ public class array {
 public static void main(String[] args) {
 	int [] array = new int[10];
 	rellenaarray(array);
+	nuevometodo1(array);
 }
 
 private static void rellenaarray(int array[]) {
@@ -11,7 +12,11 @@ private static void rellenaarray(int array[]) {
 		array[i] = (int)(Math.round(Math.random()*19)+ 1);
 		}	
 	}
-//illooooo
+private static void nuevometodo1 (int array[]) {
+	for (int i = 0; i < array.length; i++) {
+		System.out.println(array[i]);
+	}	
+}
 
 
 }
